d3b86ca77a9a139df3aa872d6d2f29634fbec6c8
Aron R <raron997@gmail.com>
2020-11-21 20:11:15 +0100

    Correcting failed commit

--- config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/discordbot', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose;
--- models/server.js
+++
@@ -1,8 +0,0 @@
-const Schema = require('mongoose').Schema;
-const db = require('../config/db');
-
-const Server = db.model('Server', {
-  discordId: String,
-});
-
-module.exports = Server;

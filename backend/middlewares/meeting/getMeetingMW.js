d3010d714b0d2bd2fdf93e16827f93445c16ce45
Kósa Szilárd <kszilard777@gmail.com>
2020-11-22 18:21:05 +0100

    :subtaskId and :meetingId changed to name from id

--- backend/middlewares/meeting/getMeetingMW.js
+++ backend/middlewares/meeting/getMeetingMW.js
@@ -1,5 +1,5 @@
 /**
- * Find a meeting in the db using the :meetingID param
+ * Find a meeting in the db using the :meetingID param and group._id
  * the result is saved to res.locals.meeting
  */
 const requireOption = require('../requireOption');
@@ -8,13 +8,16 @@ module.exports = function (objectrepository) {
   const MeetingModel = requireOption(objectrepository, 'MeetingModel');
 
   return function (req, res, next) {
-    MeetingModel.findOne({ _id: req.params.meetingID }, (err, meeting) => {
-      if (err || !meeting) {
-        return next(err || 'meeting not found');
-      }
+    MeetingModel.findOne(
+      { name: req.params.meetingID, _group: res.locals.group._id },
+      (err, meeting) => {
+        if (err || !meeting) {
+          return next(err || 'meeting not found');
+        }
 
-      res.locals.meeting = meeting;
-      return next();
-    });
+        res.locals.meeting = meeting;
+        return next();
+      }
+    );
   };
 };
--- backend/middlewares/subtask/getSubtaskMW.js
+++ backend/middlewares/subtask/getSubtaskMW.js
@@ -1,5 +1,5 @@
 /**
- * Find a subtask in the db using the :subtaskID param
+ * Find a subtask in the db using the :subtaskID param and the project._id
  * the result is saved to res.locals.subtask
  */
 const requireOption = require('../requireOption');
@@ -8,13 +8,16 @@ module.exports = function (objectrepository) {
   const SubtaskModel = requireOption(objectrepository, 'SubtaskModel');
 
   return function (req, res, next) {
-    SubtaskModel.findOne({ _id: req.params.subtaskID }, (err, subtask) => {
-      if (err || !subtask) {
-        return next(err || 'subtask not found');
-      }
+    SubtaskModel.findOne(
+      { name: req.params.subtaskID, _project: res.locals.project._id },
+      (err, subtask) => {
+        if (err || !subtask) {
+          return next(err || 'subtask not found');
+        }
 
-      res.locals.subtask = subtask;
-      return next();
-    });
+        res.locals.subtask = subtask;
+        return next();
+      }
+    );
   };
 };
